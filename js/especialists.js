5d048823644280228d9ab529cf62ab064d77c7d6
juan9900 <juanluislauretta@gmail.com>
2023-03-17 10:47:49 -0400

    added specialists desktop section

--- js/especialists.js
+++ js/especialists.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
- $('.doctor-plus-btn').on('click',function(){
-   const infoContainer = $(this).parent().siblings();
-   $(infoContainer).toggleClass('active');
+    $('.doctor-plus-btn').on('click',toggleMenu)
+ 
+})
 
-if($(this).hasClass('active')){
-    $(this).removeClass('turn active').addClass('turnCounter');
-}else{
-    $(this).addClass('turn active').removeClass('turnCounter');
+function toggleMenu(){
+    console.log(this);
+    const infoContainer = $(this).parent().siblings();
+    $(infoContainer).toggleClass('active');
+     
+     if($(infoContainer).hasClass('active')){
+         $(this).removeClass('turnCounter active').addClass('turn');
+     }else{
+         $(this).addClass('turnCounter active').removeClass('turn');
+     }
 }
-})
-
-})
